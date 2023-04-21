50951753fc3d39f54c8e8289995d8aa33a74d954
Maxwell Hammad <maxhammad@gmail.com>
2023-04-20 20:18:24 -0400

    chore: update example

--- source/handlers/getMetadata.ts
+++ source/handlers/getMetadata.ts
@@ -4,17 +4,116 @@ import { debug } from "../util";
 import { MetaDataResponse } from "../models/MetaDataResponse";
 
 /** Used to deliver metadata to the UI to support report authoring
+ * All properties of the metadata response are optional 
  */
-export const getMetadataHandler = (
-  user: User
-): Promise<MetaDataResponse> => {
+export const getMetadataHandler = (user: User): Promise<MetaDataResponse> => {
   debug("Received getMetadata request", inspect(user));
-  return Promise.resolve({
+  const response: MetaDataResponse = {
+    visualizations: [
+      {
+        name: "Sales by Year",
+        thumbnail:
+          "https://example.com/sales-by-year-thumbnail.png",
+        id: "sales",
+      },
+      {
+        name: "Revenue vs Profit",
+        thumbnail:
+          "https://example.com/revenue-vs-profit-thumbnail.png",
+        id: "revenue",
+      },
+    ],
+    dimensions: [
+      {
+        fieldName: "Year",
+        fieldDef: "year",
+      },
+      {
+        fieldName: "Quarter",
+        fieldDef: "quarter",
+      },
+      {
+        fieldName: "Month",
+        fieldDef: "month",
+      },
+      {
+        fieldName: "Customer",
+        fieldDef: "customer",
+      }
+    ],
+    measures: [
+      {
+        fieldName: "Sales",
+        fieldDef: "sales",
+      },
+      {
+        fieldName: "Profit",
+        fieldDef: "profit",
+      },
+    ],
+    tables: [
+      {
+        name: "Sales",
+        fields: [
+          {
+            fieldName: "Year",
+            fieldType: "dimension",
+            fieldDef: "Year",
+          },
+          {
+            fieldName: "Quarter",
+            fieldType: "dimension",
+            fieldDef: "Quarter",
+          },
+          {
+            fieldName: "Month",
+            fieldType: "dimension",
+            fieldDef: "Month",
+          },
+          {
+            fieldName: "Sales",
+            fieldType: "measure",
+            fieldDef: "Sales",
+          },
+          {
+            fieldName: "Profit",
+            fieldType: "measure",
+            fieldDef: "Profit",
+          },
+        ],
+      },
+      {
+        name: "Customers",
+        fields: [
+          {
+            fieldName: "Customer",
+            fieldType: "dimension",
+            fieldDef: "Customer",
+          },
+          {
+            fieldName: "State",
+            fieldType: "dimension",
+            fieldDef: "State",
+          },
+          {
+            fieldName: "City",
+            fieldType: "dimension",
+            fieldDef: "City",
+          },
+          {
+            fieldName: "Sales",
+            fieldType: "measure",
+            fieldDef: "Sales",
+          },
+        ],
+      },
+    ],
     filterFields: [
       {
         fieldName: "Year",
         fieldDef: "Year",
       },
     ],
-  });
+  };
+  return Promise.resolve(response);
 };
--- source/models/MetaDataResponse.ts
+++ source/models/MetaDataResponse.ts
@@ -2,5 +2,24 @@ import { Field } from "./Fields";
 
 export type MetaDataResponse = {
   /** List of fields available to filter on */
-  filterFields: Field[];
+  filterFields?: Field[];
+  visualizations?: {
+    /** Name of the visualization */
+    name: string;
+    /** URL of a thumbnail */
+    thumbnail?: string;
+    /** Unique idenfitier for the visualization */
+    id: string;
+  }[],
+  /** List of tables available to query */
+  tables?: {
+    /** Name of the table */
+    name: string;
+    /** List of fields available in the table */
+    fields: Field[];
+  }[];
+  /** List of dimensions available to query */
+  dimensions?: Field[],
+  /** List of measures available to query */
+  measures?: Field[]
 }
